d80685b678dc6b40c8ba46d0ef87aeaa4384b308
dasha akhten <dasha.akhten@gmail.com>
2020-05-04 11:23:12 -0700

    binary

--- dsa-recursion.js
+++ dsa-recursion.js
@@ -11,3 +11,15 @@ function sheepCounting(num) {
   sheepCounting(num - 1);
 }
 sheepCounting(3);
+
+//11
+
+function binaryRep(num) {
+  //base case
+  if (num <= 0) {
+    return "";
+  }
+  const binary = num % 2;
+  return binaryRep(Math.floor(num / 2)) + binary;
+}
+console.log(binaryRep(5));
